c49d7c9c776d9dbe586fc3dd93b7c441aef4e36d
evescn <519518995@qq.com>
2025-02-18 10:22:37 +0800

    add

--- eslint.config.js
+++ eslint.config.js
@@ -16,4 +16,32 @@ export default [
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
   skipFormatting,
+
+  // 自定义规则
+  {
+    name: 'app/custom-rules',
+    rules: {
+      // prettier 规则
+      'prettier/prettier': [
+        'warn',
+        {
+          singleQuote: true, // 单引号
+          semi: false, // 无分号
+          printWidth: 80, // 每行宽度至多 80 字符
+          trailingComma: 'none', // 不加对象|数组最后逗号
+          endOfLine: 'auto', // 兼容不同系统的换行符
+        },
+      ],
+
+      // Vue 规则
+      // 'vue/multi-word-component-names': [
+      //   'warn',
+      //   { ignores: ['index'] }, // 忽略 index.vue 组件名称
+      // ],
+      // 'vue/no-setup-props-destructure': 'off', // 允许 props 解构
+
+      // JavaScript 规则
+      'no-undef': 'error', // 禁止未定义变量
+    },
+  },
 ]
---
+++ src/views/login/LoginPage.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {ref} from 'vue'
+
+let a = ref({
+  username: '',
+  password: ''
+});
+
+</script>
+<template>
+  login
+  <div >
+    <input v-model="a.username" placeholder="Username">
+    <input v-model="a.password" placeholder="Password" type="password">
+    <button @click="login">Login</button>
+  </div>
+</template>
