62349b30b8dd18b3379c1ccf762bb5657a534d3e
meng.gao <meng.gao@dayuan1997.com>
2025-03-05 18:58:09 +0800

    add

--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@ import ArcoVue from '@arco-design/web-vue'
 import ArcoVueIcon from '@arco-design/web-vue/es/icon'
 import App from './App.vue'
 import '@arco-design/web-vue/dist/arco.css'
-// import '@arco-design/web-vue/dist/arco-dark.css'
 import router from './router'
 import pinia from '@/stores/index'
 import '@/assets/main.scss'
 
+// 导入 自定义 button
+import CButton from '@/components/CButton'
+import MainHead from '@/components/MainHead'
+
 const app = createApp(App)
+//自定义button 注册
+app.component('c-button', CButton)
+app.component('MainHead', MainHead)
 app.use(ArcoVue)
 app.use(pinia)
 app.use(router)
--- src/views/k8s/cluster/node/index.vue
+++ src/views/k8s/cluster/node/index.vue
@@ -1,5 +1,28 @@
 <script setup></script>
 
 <template>
-  <div>home</div>
+    <MainHead searchDescribe="请输入" @dataList="getNodeList" @searchChange="getSearchValue" />
+    <a-card :bodyStyle="{ padding: '10px' }">
+        <a-table :columns="columns" :dataSource="nodeList" :loading="appLoading" :pagination="pagination" style="font-size: 12px" @change="handleTableChange">
+            <template #bodyCell="{ column, record }">
+                <template v-if="column.dataIndex === 'name'">
+                    <span style="font-weight: bold">{{ record.metadata.name }}</span>
+                    <br />
+                    <span style="color: rgb(84, 138, 238)">{{ record.status.addresses[0].address }}</span>
+                </template>
+                <template v-if="column.dataIndex === 'standard'">
+                    <span>{{ record.status.capacity.cpu }}核{{ specTrans(record.status.capacity.memory) }}G</span>
+                </template>
+                <template v-if="column.dataIndex === 'podCidr'">
+                    <a-tag color="geekblue">{{ record.spec.podCIDR }}</a-tag>
+                </template>
+                <template v-if="column.dataIndex === 'version'">
+                    <span style="color: rgb(13, 173, 231)">{{ record.status.nodeInfo.kubeletVersion }} </span>
+                </template>
+                <template v-if="column.dataIndex === 'creationTimestamp'">
+                    <a-tag color="gray">{{ timeTrans(record.metadata.creationTimestamp) }}</a-tag>
+                </template>
+            </template>
+        </a-table>
+    </a-card>
 </template>
